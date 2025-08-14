9a2fa04b3fa09eaba94a610dc52f08c30ee0edc9
Alessia Bedeschi <alessia.bedeschi01@gmail.com>
2025-08-14 20:15:38 +0200

    SAT model updated

--- source/SAT/constraints.py
+++ source/SAT/constraints.py
@@ -1,110 +1,86 @@
 from z3 import *
 from itertools import combinations
 
+################ CARDINALITY CONSTRAINTS ################
 
-## --------- PAIRWISE ENCODING ---------
-
-def at_least_one_np(bool_vars):
+def at_least_one(bool_vars):
   return Or(bool_vars)
 
-def at_most_one_np(bool_vars):
+def at_most_one(bool_vars):
   return And([Not(And(a, b)) for a, b in combinations(bool_vars, 2)])
 
-def exactly_one_np(bool_vars):
-  return And(at_most_one_np(bool_vars), at_least_one_np(bool_vars))
+def exactly_one(bool_vars):
+  #return And(at_most_one(bool_vars), at_least_one(bool_vars))
+  return PbEq([(x,1) for x in bool_vars], 1)
 
+
+#def at_most_k_np(bool_vars, k):
+#    return And([Or([Not(x) for x in X]) for X in combinations(bool_vars, k + 1)])
 
 def at_most_k_np(bool_vars, k):
-    return And([Or([Not(x) for x in X]) for X in combinations(bool_vars, k + 1)])
+    return PbLe([(x, 1) for x in bool_vars], k) 
 
-# def at_most_k_np(bool_vars, k):
-#     return PbLe([(x, 1) for x in bool_vars], k) 
+#def at_least_k_np(bool_vars, k):
+#    return at_most_k_np([Not(var) for var in bool_vars], len(bool_vars)-k)
 
 def at_least_k_np(bool_vars, k):
-    return at_most_k_np([Not(var) for var in bool_vars], len(bool_vars)-k)
-
-# def at_least_k_np(bool_vars, k):
-#     return PbGe([(var, 1) for var in bool_vars], k)
+    return PbGe([(var, 1) for var in bool_vars], k)
 
 
-## ---------- CONSTRAINTS ----------
 
-# (1) Teams in a match must be different
-def constraint_diff_teams_in_a_match(H,A,Teams,Weeks,Periods,s):
+#######################  CONSTRAINTS  #######################
+
+# (1) every team plays with every other team only once
+def constraint_each_pair_once(M, n_teams, Weeks, Periods, s):
+    for i in range(n_teams):
+        for j in range(i+1, n_teams):
+            s.add(exactly_one([M[w][p][i][j] for w in Weeks for p in Periods]))
+
+
+# (2) every team plays once a week
+def constraint_one_match_per_week(M, n_teams, Weeks, Periods, s):
+    for i in range(n_teams):
+        for w in Weeks:
+            s.add(exactly_one([
+                M[w][p][i][j] if i < j else M[w][p][j][i]
+                for p in Periods
+                for j in range(n_teams) if j != i
+            ]))
+
+# (3) every team plays at most twice in the same period over the tournament
+def constraint_max_two_per_period(M, n_teams, Weeks, Periods, s):
+    for t in range(n_teams):
+        for p in Periods:
+            matches = []
+            for w in Weeks:
+                for j in range(t+1, n_teams):
+                    matches.append(M[w][p][t][j])
+                for i in range(t):
+                    matches.append(M[w][p][i][t])
+            s.add(at_most_k_np(matches, 2))
+
+# (4) one match per period
+def constraint_one_match_per_slot(M, n_teams, Weeks, Periods, s):
     for w in Weeks:
         for p in Periods:
-            for t in Teams:
-                s.add(Not(And(H[w][p][t], A[w][p][t])))
+            slot_matches = [M[w][p][i][j] for i in range(n_teams) 
+                                          for j in range(i+1, n_teams)]
+            s.add(exactly_one(slot_matches))
 
 
-# (2) Each team plays exactly once per week (it can be either at home or away)
-def constraint_only_once_in_a_week(H,A,Teams,Weeks,Periods,s):
+
+################ SYMMETRY BREAKING CONSTRAINTS ################
+
+
+# (sb1) The first match (first week and period) is between the first and the second teams (0-1)
+def sb1(M, s):
+    s.add(M[0][0][0][1])
+
+
+# (sb2) Fix opponents order for team 0
+def sb2(M, n_teams, Weeks, Periods, s):
     for w in Weeks:
-        for t in Teams:
-            occurrence = [H[w][p][t] for p in Periods] + [A[w][p][t] for p in Periods]
-            s.add(exactly_one_np(occurrence))
+        opponent = w + 1  # team 1 in week 0, team 2 in week 1, etc.
+        if opponent < n_teams:
+            s.add(Or([M[w][p][0][opponent] for p in Periods]))
 
-
-# (3) Each team plays at most twice in the same period
-def constraint_at_most_twice_in_a_period(H,A,Teams,Weeks,Periods,s):
-    for t in Teams:
-        for p in Periods:
-            occurence = [H[w][p][t] for w in Weeks] + [A[w][p][t] for w in Weeks]
-            s.add(at_most_k_np(occurence, 2))
-
-
-# (4) Each match {t1, t2} occurs exactly once
-def constraint_only_once_each_match(H,A,Teams,Weeks,Periods,s):
-    for t1 in Teams:
-        for t2 in Teams:
-            if t1 < t2:
-                occurrences = []
-                for w in Weeks:
-                    for p in Periods:
-                        # let's save all occurences where t1 plays home and t2 away
-                        occurrences.append(And(H[w][p][t1], A[w][p][t2]))
-                        # let's save all occurences where t2 plays home and t1 away
-                        occurrences.append(And(H[w][p][t2], A[w][p][t1]))
-
-                s.add(exactly_one_np(occurrences))
-
-
-# (5) Exactly one match per slot
-def constraint_one_match_per_slot(H, A, Teams, Weeks, Periods, s):
-    for w in Weeks:
-        for p in Periods:
-            home_vars = [H[w][p][t] for t in Teams]
-            away_vars = [A[w][p][t] for t in Teams]
-            s.add(exactly_one_np(home_vars))
-            s.add(exactly_one_np(away_vars))
-
-                
-
-## ---------- SYMMETRY BREAKING CONSTRAINTS ----------
-
-# (SB1) We fix all matches of the first week
-def sb1_fixing_first_week(H,A,Teams,Weeks,Periods,s):
-    for p in Periods:
-        home_t = 2*p
-        away_t = 2*p + 1
-
-        # for each period we fix the right match
-        s.add(H[0][p][home_t] == True)
-        s.add(A[0][p][away_t] == True)
-
-        # and force the others to be false
-        for t in Teams:
-            if t != home_t:
-                s.add(H[0][p][t] == False)
-            if t != away_t:
-                s.add(A[0][p][t] == False)
-
-
-# (SB2) If team_i >= team_j, then team_i cannot be at home and team_j away
-def sb2_fixing_team_order(H,A,Teams,Weeks,Periods,s):
-    for w in Weeks:
-        for p in Periods:
-            for i in Teams:
-                for j in Teams:
-                    if i >= j:
-                        s.add(Not(And(H[w][p][i], A[w][p][j])))
--- source/SAT/sat_model.py
+++ source/SAT/sat_model.py
@@ -1,80 +1,82 @@
 import time, json, math, os
 from z3 import *
+from z3 import Solver, unsat, unknown, sat
 from source.SAT.constraints import *
 
 
 # ---------------------------------------------------------------------
-# Parameters setup
+#                         Parameters setup
 # ---------------------------------------------------------------------
+
 def get_params(num_teams):
     if num_teams % 2:
-        raise ValueError("N must be even")
+        raise ValueError("Number of teams must be even")
     return num_teams, num_teams - 1, num_teams // 2
-           # n teams, W weeks, P periods
- 
- 
-# ---------------------------------------------------------------------
-# Variables setup
-# ---------------------------------------------------------------------
-def create_H_A_variables(Teams, Weeks, Periods):
-  
-    H = [[[Bool(f"H_{w}_{p}_{t}") for t in Teams] for p in Periods] for w in Weeks]
-    A = [[[Bool(f"A_{w}_{p}_{t}") for t in Teams] for p in Periods] for w in Weeks]
+#          n teams,   W weeks,       P periods
 
-    return H, A
+
+# ---------------------------------------------------------------------
+#                          Variables setup
+# ---------------------------------------------------------------------
+def create_variables(Teams, Weeks, Periods):
+    n = len(Teams)
+
+    # m[w][p][i][j] True if i vs j in week w, period p
+    M = [[[[ Bool(f"M_{w}_{p}_{i}_{j}") if i < j else None for j in range(n)] for i in range(n)] for p in Periods] for w in Weeks]
+
+    return M
 
 
 # ---------------------------------------------------------------------
 # Extract solution
 # ---------------------------------------------------------------------
-def extract_solution(model, H, A, Teams, Weeks, Periods):
-    """
-    To extract the solution and built the solution Periods x Weeks (Team_h x Team_a).
-    """
+def extract_solution(model, M, Weeks, Periods, n):
     sol = []
     for p in Periods:
-        row = []
+        period_row = []
         for w in Weeks:
-            home_team = None
-            away_team = None
-            for t in Teams:
-                if is_true(model.evaluate(H[w][p][t], model_completion=True)): #assume ungrounded variables to be False 
-                    home_team = t + 1
-                if is_true(model.evaluate(A[w][p][t], model_completion=True)):
-                    away_team = t + 1
-            row.append([home_team, away_team])
-        sol.append(row)
+            match_found = False
+            for i in range(n):
+                for j in range(i+1, n):
+                    if is_true(model.evaluate(M[w][p][i][j], model_completion=True)):
+                        period_row.append([i+1, j+1])
+                        match_found = True
+                        break
+                if match_found:
+                    break
+            if not match_found:
+                period_row.append([None, None])
+        sol.append(period_row)
     return sol
+
 
 
 # ---------------------------------------------------------------------
 # Build Result
 # ---------------------------------------------------------------------
-def build_result(time_elapsed, res, model, H, A, Teams, Weeks, Periods):
+def build_result(time_elapsed, res, model, M, Weeks, Periods, n):
+    """ 
+    Build .json file
     """
-    To build .json file
-    """
-    if time_elapsed > 300 or res != sat:
+    time_int = math.floor(time_elapsed)
+
+    if res == sat:
+        sol = extract_solution(model, M, Weeks, Periods, n)
+        return {
+            "time": time_int,
+            "optimal": True,
+            "obj": None,
+            "sol": sol
+        }
+
+    else:
         return {
             "time": 300,
             "optimal": False,
             "obj": None,
             "sol": None
         }
-
-    time_int = math.floor(time_elapsed)
-    optimal = (res == sat)
-    obj = None  # no objective function
-
-    sol = extract_solution(model, H, A, Teams, Weeks, Periods) if optimal else None
-
-
-    return {
-        "time": time_int,
-        "optimal": optimal,
-        "obj": obj,
-        "sol": sol
-    }
+  
 
 
 # ---------------------------------------------------------------------
@@ -84,16 +86,14 @@ def print_schedule(sol):
     print("[")
     for period in sol:
         print("  [", end="")
-        print(", ".join(f"[{match[0]}, {match[1]}]" for match in period), end="")
+        for match in period:
+            print(f"[{match[0]}, {match[1]}] , ", end="")
         print("]")
     print("]")
 
 
-# ---------------------------------------------------------------------
-# Solving Routine
-# ---------------------------------------------------------------------
+
 def run_single_instance(num_teams, use_sb=False, single=True):
-    
     # Get the parameters
     num_teams, num_weeks, num_periods = get_params(num_teams)
     print(f"{'-'*60}\nRunning SAT instance with {num_teams} teams, sb = {use_sb}\n{'-'*60}")
@@ -102,66 +102,65 @@ def run_single_instance(num_teams, use_sb=False, single=True):
     Weeks = list(range(num_weeks))
     Periods = list(range(num_periods))
 
-    # Create the variables, one for the teams who play home and one for the one who play away
-    H, A = create_H_A_variables(Teams, Weeks, Periods)
+    # Create the variables (only P)
+    M = create_variables(Teams, Weeks, Periods)  # M[w][p][i][j]
 
     # Create the Z3 solver
     s = Solver()
+    s.set("timeout", 300_000)  # 300 seconds in milliseconds
 
-    # Set a timeout: 300 seconds (in millisecond)
-    s.set("timeout", 300_000)
-    
+    # Start timer to store (computational + solving) time
+    start_time = time.time()
+
     # ---------------------- ADD CONSTRAINTS ----------------------
-    constraint_diff_teams_in_a_match(H,A,Teams,Weeks,Periods,s)
-    constraint_only_once_in_a_week(H,A,Teams,Weeks,Periods,s)
-    constraint_at_most_twice_in_a_period(H,A,Teams,Weeks,Periods,s)
-    constraint_only_once_each_match(H,A,Teams,Weeks,Periods,s)
-    constraint_one_match_per_slot(H,A,Teams,Weeks,Periods,s)
+    constraint_each_pair_once(M, num_teams, Weeks, Periods, s)
+    constraint_one_match_per_week(M, num_teams, Weeks, Periods, s)
+    constraint_max_two_per_period(M, num_teams, Weeks, Periods, s)
+    constraint_one_match_per_slot(M, num_teams, Weeks, Periods, s)
 
     # --------------- SYMMETRY BREAKING CONSTRAINTS ---------------
-
     if use_sb:
-        sb1_fixing_first_week(H,A,Teams,Weeks,Periods,s)
-        sb2_fixing_team_order(H,A,Teams,Weeks,Periods,s)
+        sb1(M, s)
+        # sb2(M, num_teams, Weeks, Periods, s)
 
-
-    # Solve
+    # -------------------------- SOLVE ----------------------------
     print("Start solving...")
-    start_time = time.time()
     res = s.check()
+
     end_time = time.time()
     elapsed = end_time - start_time
-    
-    
-    model = s.model() if res == sat else None    
-    result = build_result(elapsed, res, model, H, A, Teams, Weeks, Periods)
 
-    # Print solution
+    model = s.model() if res == sat else None
+    result = build_result(elapsed, res, model, M, Weeks, Periods, num_teams)
+
+    # ------------------------ PRINT SOLUTION ---------------------
     if result["optimal"]:
-        print(f"Solver result: sat in {elapsed:.2f} seconds")
+        print(f"Proved to be sat in {elapsed:.2f} seconds")
         print("\nSchedule found:")
         print_schedule(result["sol"])
+    elif res == unsat:
+        print("\nNo valid schedule exists")
     else:
-        print("\nNo valid schedule found or solver timed out.")
-    
-    # Save
+        print("Solver timed out")
+
+    # ------------------------ SAVE SOLUTION ----------------------
     if single:
-        output_dir = os.path.join(os.path.dirname(__file__), "../..", "res", "SAT")
-        os.makedirs(output_dir, exist_ok=True)
+        output_dir = os.path.join(os.path.dirname(__file__), "../../res/SAT")
         out_path = os.path.join(output_dir, f"{num_teams}.json")
 
+        key = "z3_sb" if use_sb else "z3_nosb"
+
         with open(out_path, "w") as f:
-            json.dump({ "z3_sb" if use_sb else "z3_nosb": result }, f, indent=2)
-    
+            json.dump({key: result}, f, indent=2)
 
     return result
 
 
 def run_all():
-    output_dir = os.path.join(os.path.dirname(__file__), "../..", "res", "SAT")
-    os.makedirs(output_dir, exist_ok=True)
+    output_dir = os.path.join(os.path.dirname(__file__), "../../res/SAT")
+    output_dir = os.path.abspath(output_dir)
 
-    n_list = [6, 8, 10, 12, 14]
+    n_list = [6, 8, 10, 12, 14, 16]
 
     for num_teams in n_list:
         results = {}
@@ -169,7 +168,6 @@ def run_all():
             key = f"z3_sb" if sb else "z3_nosb"
             results[key] = run_single_instance(num_teams, sb, single=False)
 
-        
         out_path = os.path.join(output_dir, f"{num_teams}.json")
         with open(out_path, "w") as f:
             json.dump(results, f, indent=2)
